72657b4091d9f3898dbbcef644782a97654db699
itzmarcos <marcozfuwa@gmail.com>
2024-08-04 14:28:14 -0300

    area

--- PythonF/main.py
+++ PythonF/main.py
@@ -1,1 +1,17 @@
-print('Hello World!')
+def area(largura, comp):
+    r = largura * comp
+    print(f'A largura de {largura} com o comprimento de {comp} da o resultado da area de {r}')
+
+l = float(input("Digite a largura: "))
+c = float(input("Digite o comprimento: "))
+area(l, c)
+
+
+
+
+
+
+
+
+
+

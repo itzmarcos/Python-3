ae63d13e52fbe24156beed6d5e830276f2ff2fe0
itzmarcos <marcozfuwa@gmail.com>
2024-08-08 06:37:28 -0300

    02

--- PythonF/main.py
+++ PythonF/main.py
@@ -1,12 +1,28 @@
-def area(largura, comp):
+
+#01
+'''def area(largura, comp):
     r = largura * comp
     print(f'A largura de {largura} com o comprimento de {comp} da o resultado da area de {r}')
 
 l = float(input("Digite a largura: "))
 c = float(input("Digite o comprimento: "))
-area(l, c)
+area(l, c)'''
 
 
+#02
+
+'''def lin():
+    print('-' * len(msg))
+def escreva(msg):
+        lin()
+        print(f'{msg}')
+        lin()
+
+for msg in range (3):
+    msg = input('Digite algo: ')
+    escreva(msg)'''
+
+#03
 
 
 

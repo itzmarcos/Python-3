2f8bc2f703fb02814b902864f1290dab908c9e35
itzmarcos <marcozfuwa@gmail.com>
2024-08-09 13:12:31 -0300

    03

--- PythonF/main.py
+++ PythonF/main.py
@@ -23,11 +23,36 @@ for msg in range (3):
     escreva(msg)'''
 
 #03
+'''from time import sleep
+def contador(i, f, p):
+    if p < 0:
+        p*= -1
+    if p == 0:
+        p = 1
+    print(f'Contagem de {i} até {f} de {p} em {p}')
+    sleep(2.5)
+
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.5)
+            cont += p
+        print('END')
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.5)
+            cont -= p
+        print('END')
 
 
 
-
-
-
-
-
+contador(1, 10, 1)
+contador(10, 0, 2)
+print('Agora você escolhe!')
+ini = int(input('Inicio: '))
+fim = int(input('Fim: '))
+pas = int(input('Passo: '))
+contador(ini, fim, pas)'''

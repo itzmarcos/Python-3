6e6cfda1013636256cf8a9c89830be77efb1b251
itzmarcos <marcozfuwa@gmail.com>
2024-08-10 09:47:03 -0300

    update

--- O_Arquivos/Organizador.py
+++ O_Arquivos/Organizador.py
@@ -4,21 +4,21 @@ from tkinter.filedialog import askdirectory
 rota = askdirectory(title="Selecione uma pasta")
 
 lista_arquivos = os.listdir(rota)
-print(lista_arquivos)
 
 locais = {
         "imagens": [".png", ".jpg"],
         "planilhas": [".xlsx"],
         "pdfs": [".pdf"],
         "docx": [".docx"],
+        "txt": [".txt"],
+
     }
 
 for arquivo in lista_arquivos:
      #pdfs
      nome, extensao = os.path.splitext(f"{rota}/{arquivo}")
      for pasta in locais:
-          print(pasta)
           if extensao in locais[pasta]:
               if not os.path.exists(f"{rota}/{pasta}"):
                   os.mkdir(f"{rota}/{pasta}")
-                  os.rename(f"{rota}/{arquivo}", f"{rota}/{pasta}/{arquivo}")
+              os.rename(f"{rota}/{arquivo}", f"{rota}/{pasta}/{arquivo}")
